47745797b442c396e8eea39e4a484b2ffb322399
Mohammed Manssour <manssour.mohammed@gmail.com>
2022-04-05 23:38:39 +0400

    docs: better badges docs

--- packages/docs/components/Example.tsx
+++ packages/docs/components/Example.tsx
@@ -1,6 +1,6 @@
-import { FC } from "react";
+import { FC } from 'react';
 
-import CodeSnippet from "./CodeSnippet";
+import CodeSnippet from './CodeSnippet';
 
 interface IExample {
   code: string;
@@ -8,7 +8,7 @@ interface IExample {
 
 const Example: FC<IExample> = ({ children, code }) => (
   <>
-    <div className="lg:flex-row lg:space-y-0 lg:space-x-4 flex flex-col items-start p-4 space-y-4 border border-gray-300 rounded-lg">
+    <div className="lg:flex-row lg:space-y-0 lg:space-x-4 not-prose flex flex-col items-start p-4 space-y-4 border border-gray-300 rounded-lg">
       {children}
     </div>
     <CodeSnippet>{code}</CodeSnippet>
