a98134478f5cbaf9305c058a42585567ac837648
Mohammed Manssour <manssour.mohammed@gmail.com>
2022-04-09 12:18:46 +0400

    update Example component to use flex wrap

--- packages/docs/components/Example.tsx
+++ packages/docs/components/Example.tsx
@@ -8,7 +8,7 @@ interface IExample {
 
 const Example: FC<IExample> = ({ children, code }) => (
   <>
-    <div className="lg:flex-row lg:space-y-0 lg:space-x-4 not-prose flex flex-col items-start p-4 space-y-4 border border-gray-300 rounded-lg">
+    <div className="not-prose flex flex-wrap gap-4 p-4 border border-gray-300 rounded-lg">
       {children}
     </div>
     <CodeSnippet>{code}</CodeSnippet>
