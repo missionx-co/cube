902a24bf975562bb3517c891dff6383976023111
Mohammed Manssour <manssour.mohammed@gmail.com>
2022-04-09 18:33:01 +0400

    docs: fix active page for MDXPages

--- packages/docs/components/Layout/MDXPage.tsx
+++ packages/docs/components/Layout/MDXPage.tsx
@@ -1,16 +1,31 @@
-import { FC, ReactNode } from 'react';
+import { FC, ReactNode, useEffect } from 'react';
 
 import Page from './Page';
+import { useSidebarContext } from './SidebarContext';
 
 interface IPage {
   title?: string;
+  page?: string;
   description?: ReactNode;
 }
 
-const MDXPage: FC<IPage> = ({ title, description, children }) => (
-  <Page title={title} description={description}>
-    <article className="lg:prose-lg max-w-none prose">{children}</article>
-  </Page>
-);
+const MDXPage: FC<IPage> = ({ title, page, description, children }) => {
+  const { setActivePage } = useSidebarContext();
+  useEffect(() => {
+    if (!page) {
+      return;
+    }
+
+    setActivePage(page);
+
+    /* eslint-disable */
+  }, [page]);
+
+  return (
+    <Page title={title} description={description}>
+      <article className="lg:prose-lg max-w-none prose">{children}</article>
+    </Page>
+  );
+};
 
 export default MDXPage;
