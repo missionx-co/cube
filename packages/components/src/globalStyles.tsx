2d183c0ff440f184bd36cb454adaf7cd590e6b8f
Mohammed Manssour <manssour.mohammed@gmail.com>
2022-04-23 21:52:24 +0400

    remove dark bg from global styles

--- packages/components/src/globalStyles.tsx
+++ packages/components/src/globalStyles.tsx
@@ -1,9 +1,10 @@
-import tw, { theme, globalStyles } from "twin.macro";
-import { globalCss } from "./stitches.config";
+import tw, { globalStyles, theme } from 'twin.macro';
+
+import { globalCss } from './stitches.config';
 
 const customStyles = {
   body: {
-    ...tw`font-sans bg-gray-900`,
+    ...tw`font-sans`,
   },
 };
 
